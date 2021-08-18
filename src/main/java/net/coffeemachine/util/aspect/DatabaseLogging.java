b5e2f8589d611848f3472aad4a1a2e6bfd026c21
konianzero <konianzero@gmail.com>
2021-08-18 20:47:21 +0300

    feat: Move logging to aspect

--- src/main/java/net/coffeemachine/util/aspect/DatabaseLogging.java
+++ src/main/java/net/coffeemachine/util/aspect/DatabaseLogging.java
@@ -4,6 +4,5 @@ import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD})
-//@Inherited
 public @interface DatabaseLogging {
 }
--- src/main/java/net/coffeemachine/util/aspect/DatabaseLoggingAspect.java
+++ src/main/java/net/coffeemachine/util/aspect/DatabaseLoggingAspect.java
@@ -2,13 +2,9 @@ package net.coffeemachine.util.aspect;
 
 import lombok.extern.slf4j.Slf4j;
 
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.*;
 
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -16,25 +12,20 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class DatabaseLoggingAspect {
 
-
     @Pointcut("@annotation(DatabaseLogging)")
-//    @Pointcut("@annotation(net.coffeemachine.util.annotation.DatabaseLogging) && execution(public * *(..))")
-//    @Pointcut("within(com.example..*) && @annotation(net.coffeemachine.util.annotation.DatabaseLogging)")
-//    @Pointcut("execution(* net.coffeemachine.service.CoffeeMachine.*(..))")
-//    @Pointcut("execution(* net.coffeemachine.service.states..*(..))")
     public void loggableMethods() {
     }
-
-//    @Before("loggableMethods()")
-//    public void logMethod(JoinPoint jointPoint) {
-//        String methodName = jointPoint.getSignature().getName();
-//        log.info("<<< Before Aspect called for method: {}", methodName);
-//    }
 
     @Around("loggableMethods()")
     public Object logCoffeeMachineActions(ProceedingJoinPoint joinPoint) throws Throwable {
         Object value = joinPoint.proceed();
-        log.info("ASP <<< Invoked: {} - Returned: {}", joinPoint.getSignature().getName(), value);
+        log.info((String) value);
         return value;
     }
+
+    @AfterThrowing(pointcut = "execution(* net.coffeemachine.service..*(..))", throwing = "ex")
+    public void logAfterThrowingAllMethods(Exception ex) {
+        log.error(ex.getMessage());
+    }
+
 }
